1e6b7c8933ce91347d32dfa422068614933b0eac
djarae <djaraestivill@gmail.com>
2021-05-20 03:40:44 -0400

    10 first excersse completed

--- problemas/p1.js
+++ problemas/p1.js
@@ -7,38 +7,109 @@
  * 4. Retornar todas las Sedes con 4° Medio que *SI* poseen la *Oferta Tecnología* en sus *Secciones A*
  */
 const data = require("./src/input-p1.json");
-// console.log(data);console.log(data.hijos.length);
 
-returnNoSons()
+console.log(returnNoSons());
+console.log(returnEspecific(4));
+console.log(contabilization());
+console.log(ejerc4());
+
 function returnNoSons(){
     //NOTA: I representa la palabra : INDEX
+    let arrayFinal = []
     for (let sedeI=0;sedeI<data.hijos.length;sedeI++){
         if (data.hijos[sedeI].hijos.length==0){
-            console.log(data.hijos[sedeI])
+            arrayFinal.push(data.hijos[sedeI]);
         }
-        // console.log("muy buenas gente");
-        // console.log("muy buenas gente");  console.log(data.hijos[sedeI]);
         for (let cursoI=0;         cursoI<data.hijos[sedeI].hijos.length;          cursoI++){
             if (data.hijos[sedeI].hijos[cursoI].hijos.length==0){
-                console.log(data.hijos[sedeI].hijos[cursoI]);
+                arrayFinal.push(data.hijos[sedeI].hijos[cursoI]);
             }
-            // console.log("muy buenas gente2");  console.log(data.hijos[sedeI].hijos[cursoI].hijos);
             for (let seccionI=0; seccionI<data.hijos[sedeI].hijos[cursoI].hijos.length;              seccionI++){
-                // console.log("muy buenas gente3"); console.log(data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos);
                 if ( data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos.length==0){
-                    console.log(data.hijos[sedeI].hijos[cursoI].hijos[seccionI]);
+                    arrayFinal.push(data.hijos[sedeI].hijos[cursoI].hijos[seccionI]);
                 }
                 for (let ofertaI=0; ofertaI<   data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos.length;              ofertaI++){
-                    // console.log("muy buenas gente55"); console.log(data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI].hijos);
                     if (data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI].hijos.length==0){
-                        console.log(data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI])
+                        arrayFinal.push(data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI]);
                     }
                 }
-
-
             }
         }
     }
+    return arrayFinal
 }
-// console.log(data.hijos[0].hijos[0].hijos[0]);
+
+
+
+
+function returnEspecific(esp){
+    let arrayFinal = []
+    for (let sedeI=0;sedeI<data.hijos.length;sedeI++){
+        if (data.hijos[sedeI].hijos.length==esp){
+            arrayFinal.push(data.hijos[sedeI]);
+        }
+        for (let cursoI=0;         cursoI<data.hijos[sedeI].hijos.length;          cursoI++){
+            if (data.hijos[sedeI].hijos[cursoI].hijos.length==esp){
+                arrayFinal.push(data.hijos[sedeI].hijos[cursoI]);
+            }
+            for (let seccionI=0; seccionI<data.hijos[sedeI].hijos[cursoI].hijos.length;              seccionI++){
+                if ( data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos.length==esp){
+                    arrayFinal.push(data.hijos[sedeI].hijos[cursoI].hijos[seccionI]);
+                }
+                for (let ofertaI=0; ofertaI<   data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos.length;              ofertaI++){
+                    if (data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI].hijos.length==esp){
+                        arrayFinal.push(data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI]);
+                    }
+                }
+            }
+        }
+    }
+    return arrayFinal
+}
+
+
+
+
+function  contabilization(){
+    let count = 0;
+    for (let sedeI=0;sedeI<data.hijos.length;sedeI++){
+        count=count+1;
+        for (let cursoI=0;         cursoI<data.hijos[sedeI].hijos.length;          cursoI++){
+            count=count+1;
+            for (let seccionI=0; seccionI<data.hijos[sedeI].hijos[cursoI].hijos.length;              seccionI++){
+                count=count+1;
+                for (let ofertaI=0; ofertaI<   data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos.length;              ofertaI++){
+                   count=count+1;
+                }
+            }
+        }
+    }
+    return   count
+}
+
+// 4. Retornar todas las Sedes con 4° Medio que *SI* poseen la *Oferta Tecnología* en sus *Secciones A*
+function ejerc4(){
+    let arrayFinal = []
+        for (let sedeI=0;sedeI<data.hijos.length;sedeI++){
+            for (let cursoI=0;         cursoI<data.hijos[sedeI].hijos.length;          cursoI++){
+                if (data.hijos[sedeI].hijos[cursoI].nombre=="4 Medio"){
+                    for (let seccionI=0; seccionI<data.hijos[sedeI].hijos[cursoI].hijos.length;              seccionI++){
+                        if (data.hijos[sedeI].hijos[cursoI].hijos[seccionI].nombre=="A"){
+                            for (let ofertaI=0; ofertaI<   data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos.length;              ofertaI++){
+                                if (data.hijos[sedeI].hijos[cursoI].hijos[seccionI].hijos[ofertaI].nombre=="Tecnología"){
+                                    arrayFinal.push(data.hijos[sedeI].nombre);
+                                }
+                            }
+                        }
+                       
+                    }
+                }
+
+               
+            }
+        }
+        return   arrayFinal
+}
+
+
 
