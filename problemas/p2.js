1db4f6e26d7c8c5747ec8cf430c1ecfc6fb148f8
djarae <djaraestivill@gmail.com>
2021-05-20 04:05:36 -0400

    12 csv parse

--- problemas/p2.js
+++ problemas/p2.js
@@ -5,10 +5,21 @@
  * en un arból de estructura similar al problema de p1 utilizando la estructura de Nodo.js
  */
 
+
 const Nodo = require("./src/Nodo");
-const raiz = new Nodo("root", "Raíz");
+const raiz = new Nodo("root", "Raíz",[]);
+const neatCsv = require('neat-csv');
 
 //Agregar a raiz toda la estructura solicitada.
 //...
+const fs = require('fs')
+
+fs.readFile('./src/input-p2.csv', async (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(await neatCsv(data))
+})
 
 console.log(raiz);
